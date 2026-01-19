cecfec9e3aac85bad66ecb6caef0277fd40a030d
vivek ranjan <vivek49ranjan@gmail.com>
2026-01-19 21:14:29 +0530

    Update red_black.c

--- CFS/red_black.c
+++ CFS/red_black.c
@@ -1,131 +1,86 @@
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
-    Node* parent;
-    int colour;
-    Node(int data) : data(data), colour(1), left(nullptr), right(nullptr), parent(nullptr) {}
-};
-int max(int a, intb){
-    return a>b ? a: b;
-}
-
-
-int Node_depth(Node* Root){
-    if(!Node) return 0;
-    else return max(Node_depth(Root->left) , Node_depth(Root->right));
-}
-
-void left_rotate(Node* &root, Node* x) {
-    Node* y = x->right;
+void left_rotate(rb_node **root, rb_node *x) {
+    rb_node *y = x->right;
     x->right = y->left;
-
-    if (y->left)
-        y->left->parent = x;
-
+    if (y->left) y->left->parent = x;
     y->parent = x->parent;
-
-    if (!x->parent)
-        root = y;
-    else if (x == x->parent->left)
-        x->parent->left = y;
-    else
-        x->parent->right = y;
-
+    if (!x->parent) *root = y;
+    else if (x == x->parent->left) x->parent->left = y;
+    else x->parent->right = y;
     y->left = x;
     x->parent = y;
 }
 
-void rightRotate(Node* &root, Node* y) {
-    Node* x = y->left;
+void right_rotate(rb_node **root, rb_node *y) {
+    rb_node *x = y->left;
     y->left = x->right;
-
-    if (x->right)
-        x->right->parent = y;
-
+    if (x->right) x->right->parent = y;
     x->parent = y->parent;
-
-    if (!y->parent)
-        root = x;
-    else if (y == y->parent->left)
-        y->parent->left = x;
-    else
-        y->parent->right = x;
-
+    if (!y->parent) *root = x;
+    else if (y == y->parent->left) y->parent->left = x;
+    else y->parent->right = x;
     x->right = y;
     y->parent = x;
 }
 
-void fixInsert(Node* &root, Node* z) {
-    while (z != root && z->parent->color == RED) {
-
-        Node* parent = z->parent;
-        Node* grandparent = parent->parent;
-
-        if (parent == grandparent->left) {
-            Node* uncle = grandparent->right;
-
+void fix_insert(rb_node **root, rb_node *z) {
+    while (z != *root && z->parent->color == RED) {
+        if (z->parent == z->parent->parent->left) {
+            rb_node *uncle = z->parent->parent->right;
             if (uncle && uncle->color == RED) {
-                parent->color = BLACK;
+                z->parent->color = BLACK;
                 uncle->color = BLACK;
-                grandparent->color = RED;
-                z = grandparent;
-            }
-            else {
-                if (z == parent->right) {
-                    z = parent;
-                    leftRotate(root, z);
+                z->parent->parent->color = RED;
+                z = z->parent->parent;
+            } else {
+                if (z == z->parent->right) {
+                    z = z->parent;
+                    left_rotate(root, z);
                 }
-                parent->color = BLACK;
-                grandparent->color = RED;
-                rightRotate(root, grandparent);
+                z->parent->color = BLACK;
+                z->parent->parent->color = RED;
+                right_rotate(root, z->parent->parent);
             }
-        }
-
-        else {
-            Node* uncle = grandparent->left;
-
+        } else {
+            rb_node *uncle = z->parent->parent->left;
             if (uncle && uncle->color == RED) {
-                parent->color = BLACK;
+                z->parent->color = BLACK;
                 uncle->color = BLACK;
-                grandparent->color = RED;
-                z = grandparent;
-            }
-            else {
-                if (z == parent->left) {
-                    z = parent;
-                    rightRotate(root, z);
+                z->parent->parent->color = RED;
+                z = z->parent->parent;
+            } else {
+                if (z == z->parent->left) {
+                    z = z->parent;
+                    right_rotate(root, z);
                 }
-                parent->color = BLACK;
-                grandparent->color = RED;
-                leftRotate(root, grandparent);
+                z->parent->color = BLACK;
+                z->parent->parent->color = RED;
+                left_rotate(root, z->parent->parent);
             }
         }
     }
-
-    root->color = BLACK; 
+    (*root)->color = BLACK;
 }
-void insert(Node* &root, int key) {
-    Node* z = new Node(key);
-    Node* y = nullptr;
-    Node* x = root;
+
+void rb_insert(rb_node **root, process *p) {
+    rb_node *z = (rb_node*)malloc(sizeof(rb_node));
+    z->vruntime = p->vruntime;
+    z->task = p;
+    z->left = z->right = z->parent = NULL;
+    z->color = RED;
+
+    rb_node *y = NULL;
+    rb_node *x = *root;
 
     while (x) {
         y = x;
-        if (z->key < x->key)
-            x = x->left;
-        else
-            x = x->right;
+        if (z->vruntime < x->vruntime) x = x->left;
+        else x = x->right;
     }
 
     z->parent = y;
+    if (!y) *root = z;
+    else if (z->vruntime < y->vruntime) y->left = z;
+    else y->right = z;
 
-    if (!y)
-        root = z;
-    else if (z->key < y->key)
-        y->left = z;
-    else
-        y->right = z;
-
-    fixInsert(root, z);
+    fix_insert(root, z);
 }
