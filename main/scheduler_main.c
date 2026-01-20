e19cc0a028410720e27a51f1d0c6682672a7472d
Pranjul Singhal <pranjulsinghal2006@gmail.com>
2026-01-20 23:28:30 +0530

    Update scheduler_main.c

--- main/scheduler_main.c
+++ main/scheduler_main.c
@@ -42,6 +42,29 @@ typedef struct {
     double duration;
 } gantt_step;
 
+typedef struct process_fcfs {
+    int pid;
+    int arrival_time;
+    int burst_time;
+
+    int remaining_time;
+    int executed_time;
+    int start_time;
+    int completion_time;
+
+    int io_start;
+    int io_duration;
+    int io_remaining;
+    int in_io;
+
+    struct process_fcfs *next;
+} Process;
+
+typedef struct {
+    Process *front;
+    Process *rear;
+} Queue;
+
 rb_node *runqueue = NULL;
 int active_tasks = 0;
 
@@ -50,6 +73,113 @@ int history_count = 0;
 
 process *finished[MAX_PROC];
 int finished_count = 0;
+
+Queue fcfs_arrival_q;
+
+void init_queue(Queue *q) {
+    q->front = q->rear = NULL;
+}
+
+void enqueue(Queue *q, Process *p) {
+    p->next = NULL;
+    if (!q->rear) {
+        q->front = q->rear = p;
+        return;
+    }
+    q->rear->next = p;
+    q->rear = p;
+}
+
+Process* dequeue(Queue *q) {
+    if (!q->front) return NULL;
+    Process *p = q->front;
+    q->front = p->next;
+    if (!q->front) q->rear = NULL;
+    p->next = NULL;
+    return p;
+}
+
+void insert_by_arrival(Queue *q, Process *p) {
+    if (!q->front || p->arrival_time < q->front->arrival_time) {
+        p->next = q->front;
+        q->front = p;
+        if (!q->rear) q->rear = p;
+        return;
+    }
+
+    Process *cur = q->front;
+    while (cur->next && cur->next->arrival_time <= p->arrival_time)
+        cur = cur->next;
+
+    p->next = cur->next;
+    cur->next = p;
+    if (!p->next) q->rear = p;
+}
+
+void fcfs_io_aware(Queue *arrival_q) {
+    Queue ready_q = {0};
+    Queue io_q = {0};
+
+    int clock = 0;
+
+    printf("\nPID | AT | BT | ST | CT | TAT | WT\n");
+    printf("---------------------------------\n");
+
+    while (arrival_q->front || ready_q.front || io_q.front) {
+        while (arrival_q->front && arrival_q->front->arrival_time <= clock)
+            enqueue(&ready_q, dequeue(arrival_q));
+
+        Process *prev = NULL, *cur = io_q.front;
+        while (cur) {
+            cur->io_remaining--;
+            if (cur->io_remaining <= 0) {
+                Process *done = cur;
+                if (prev) prev->next = cur->next;
+                else io_q.front = cur->next;
+                cur = cur->next;
+                done->in_io = 0;
+                enqueue(&ready_q, done);
+            } else {
+                prev = cur;
+                cur = cur->next;
+            }
+        }
+
+        if (!ready_q.front) {
+            clock++;
+            continue;
+        }
+
+        Process *p = dequeue(&ready_q);
+
+        if (p->start_time == -1)
+            p->start_time = clock;
+
+        p->remaining_time--;
+        clock++;
+
+        if (!p->in_io && p->remaining_time == p->burst_time - p->io_start) {
+            p->in_io = 1;
+            p->io_remaining = p->io_duration;
+            enqueue(&io_q, p);
+            continue;
+        }
+
+        if (p->remaining_time == 0) {
+            p->completion_time = clock;
+            int tat = p->completion_time - p->arrival_time;
+            int wt = tat - p->burst_time;
+
+            printf("%3d | %2d | %2d | %2d | %2d | %3d | %2d\n",
+                   p->pid, p->arrival_time, p->burst_time,
+                   p->start_time, p->completion_time, tat, wt);
+            free(p);
+        } else {
+            enqueue(&ready_q, p);
+        }
+    }
+}
+
 
 void left_rotate(rb_node **root, rb_node *x) {
     rb_node *y = x->right;
@@ -272,6 +402,9 @@ int main() {
     }
 
     int pid, at, bt;
+
+    init_queue(&fcfs_arrival_q);
+
     printf("--- Admitting Processes ---\n");
 
     while (fscanf(fp, "%d %d %d", &pid, &at, &bt) == 3) {
@@ -280,8 +413,29 @@ int main() {
         rb_insert(&runqueue, p);
         active_tasks++;
         printf("Admitted PID %d\n", pid);
+
+        Process *fp_proc = malloc(sizeof(Process));
+        fp_proc->pid = pid;
+        fp_proc->arrival_time = at;
+        fp_proc->burst_time = bt;
+        fp_proc->remaining_time = bt;
+        fp_proc->executed_time = 0;
+        fp_proc->start_time = -1;
+        fp_proc->completion_time = 0;
+        fp_proc->io_start = bt / 2;
+        fp_proc->io_duration = 2;
+        fp_proc->io_remaining = 0;
+        fp_proc->in_io = 0;
+        fp_proc->next = NULL;
+
+        insert_by_arrival(&fcfs_arrival_q, fp_proc);
+
+        printf("Admitted PID %d\n", pid);
     }
     fclose(fp);
+
+    printf("\n--- Starting FCFS Scheduler ---\n");
+    fcfs_io_aware(&fcfs_arrival_q);
 
     printf("\n--- Starting CFS Scheduler ---\n");
     cfs_schedule();
