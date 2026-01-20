88ca2085941e09e09379c64177fe727abdb55d8b
MohakGupta1411 <mohakg1411@gmail.com>
2026-01-20 22:52:03 +0530

    Updated the sjf implementation by adding aging factor in order to reduce starvation

--- SJF/sjf.c
+++ SJF/sjf.c
@@ -2,59 +2,69 @@
 #include <stdlib.h>
 
 #define MAX_PROC 100
+#define AGING_FACTOR 1
 
 typedef struct
 {
     int pid;
-    int arrival_time;
-    int burst_time;
-    int start_time;
-    int completion_time;
-    int waiting_time;
-    int turnaround_time;
-    int response_time;
+    int arrival;
+    int burst;
+    int start;
+    int completion;
+    int waiting;
+    int turnaround;
+    int response;
+    int age;
 } Process;
 
 typedef struct
 {
-    Process heap[MAX_PROC];
+    Process data[MAX_PROC];
     int size;
-} MinHeap;
+} ReadyQueue;
 
 typedef struct
 {
     int pid;
     int start;
     int end;
-} GanttStep;
+} Gantt;
 
-void createProcess(Process *p, int id, int arr, int brst)
+void initProcess(Process *p, int id, int at, int bt)
 {
     p->pid = id;
-    p->arrival_time = arr;
-    p->burst_time = brst;
-    p->start_time = -1;
-    p->completion_time = 0;
-    p->waiting_time = 0;
-    p->turnaround_time = 0;
-    p->response_time = 0;
+    p->arrival = at;
+    p->burst = bt;
+    p->start = -1;
+    p->completion = 0;
+    p->waiting = 0;
+    p->turnaround = 0;
+    p->response = 0;
+    p->age = 0;
 }
 
-int compareArrival(const void *a, const void *b)
+int sortByArrival(const void *a, const void *b)
 {
-    const Process *p1 = (const Process *)a;
-    const Process *p2 = (const Process *)b;
-    if (p1->arrival_time != p2->arrival_time)
-        return p1->arrival_time - p2->arrival_time;
+    Process *p1 = (Process *)a;
+    Process *p2 = (Process *)b;
+
+    if (p1->arrival != p2->arrival)
+        return p1->arrival - p2->arrival;
+
     return p1->pid - p2->pid;
 }
 
-int higherPriority(Process a, Process b)
+int hasHigherPriority(Process a, Process b)
 {
-    if (a.burst_time != b.burst_time)
-        return a.burst_time < b.burst_time;
-    if (a.arrival_time != b.arrival_time)
-        return a.arrival_time < b.arrival_time;
+    int effectiveA = a.burst - a.age * AGING_FACTOR;
+    int effectiveB = b.burst - b.age * AGING_FACTOR;
+
+    if (effectiveA != effectiveB)
+        return effectiveA < effectiveB;
+
+    if (a.arrival != b.arrival)
+        return a.arrival < b.arrival;
+
     return a.pid < b.pid;
 }
 
@@ -65,139 +75,147 @@ void swap(Process *a, Process *b)
     *b = temp;
 }
 
-void heapifyUp(MinHeap *h, int idx)
+void heapifyUp(ReadyQueue *rq, int index)
 {
-    while (idx > 0)
+    while (index > 0)
     {
-        int parent = (idx - 1) / 2;
-        if (higherPriority(h->heap[idx], h->heap[parent]))
+        int parent = (index - 1) / 2;
+
+        if (hasHigherPriority(rq->data[index], rq->data[parent]))
         {
-            swap(&h->heap[idx], &h->heap[parent]);
-            idx = parent;
+            swap(&rq->data[index], &rq->data[parent]);
+            index = parent;
         }
         else
             break;
     }
 }
 
-void heapifyDown(MinHeap *h, int idx)
+void heapifyDown(ReadyQueue *rq, int index)
 {
     while (1)
     {
-        int smallest = idx;
-        int left  = 2 * idx + 1;
-        int right = 2 * idx + 2;
-        if (left < h->size && higherPriority(h->heap[left], h->heap[smallest]))
-            smallest = left;
-        if (right < h->size && higherPriority(h->heap[right], h->heap[smallest]))
-            smallest = right;
-        if (smallest != idx)
+        int best = index;
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
+
+        if (left < rq->size && hasHigherPriority(rq->data[left], rq->data[best]))
+            best = left;
+
+        if (right < rq->size && hasHigherPriority(rq->data[right], rq->data[best]))
+            best = right;
+
+        if (best != index)
         {
-            swap(&h->heap[idx], &h->heap[smallest]);
-            idx = smallest;
+            swap(&rq->data[index], &rq->data[best]);
+            index = best;
         }
         else
             break;
     }
 }
 
-void pushHeap(MinHeap *h, Process p)
+void pushProcess(ReadyQueue *rq, Process p)
 {
-    if (h->size >= MAX_PROC)
-        return;
-    h->heap[h->size] = p;
-    heapifyUp(h, h->size);
-    h->size++;
+    rq->data[rq->size] = p;
+    heapifyUp(rq, rq->size);
+    rq->size++;
 }
 
-Process popHeap(MinHeap *h)
+Process popProcess(ReadyQueue *rq)
 {
-    Process min = h->heap[0];
-    h->heap[0] = h->heap[h->size - 1];
-    h->size--;
-    heapifyDown(h, 0);
-    return min;
+    Process top = rq->data[0];
+    rq->data[0] = rq->data[rq->size - 1];
+    rq->size--;
+    heapifyDown(rq, 0);
+    return top;
 }
 
-void printGanttChart(GanttStep g[], int n)
+void increaseAge(ReadyQueue *rq)
 {
-    int i;
-    printf("\n--- Gantt Chart ---\n ");
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < rq->size; i++)
+        rq->data[i].age++;
+}
+
+void printGantt(Gantt chart[], int count)
+{
+    printf("\nGantt Chart\n ");
+
+    for (int i = 0; i < count; i++)
         printf("-------");
+
     printf("\n|");
-    for (i = 0; i < n; i++)
+
+    for (int i = 0; i < count; i++)
     {
-        if (g[i].pid == -1)
+        if (chart[i].pid == -1)
             printf(" IDLE |");
         else
-            printf("  P%d  |", g[i].pid);
+            printf("  P%d  |", chart[i].pid);
     }
+
     printf("\n ");
-    for (i = 0; i < n; i++)
+
+    for (int i = 0; i < count; i++)
         printf("-------");
-    printf("\n%d", g[0].start);
-    for (i = 0; i < n; i++)
-        printf("%7d", g[i].end);
+
+    printf("\n%d", chart[0].start);
+
+    for (int i = 0; i < count; i++)
+        printf("%7d", chart[i].end);
+
     printf("\n\n");
 }
 
-void SJF(Process input[], int n)
+void scheduleSJF(Process processes[], int n)
 {
-    qsort(input, n, sizeof(Process), compareArrival);
-    MinHeap rq;
-    rq.size = 0;
-    GanttStep gantt[MAX_PROC * 2];
-    int g_idx = 0;
-    Process result[MAX_PROC];
-    int res_idx = 0;
-    int time = 0;
-    int i = 0;
-    float total_wt = 0, total_tat = 0, total_rt = 0;
+    qsort(processes, n, sizeof(Process), sortByArrival);
 
-    while (res_idx < n)
+    ReadyQueue rq;
+    rq.size = 0;
+
+    Gantt chart[MAX_PROC * 2];
+    int chartSize = 0;
+
+    int time = 0, index = 0, done = 0;
+    float avgWait = 0, avgTurn = 0, avgResp = 0;
+
+    while (done < n)
     {
-        while (i < n && input[i].arrival_time <= time)
-        {
-            pushHeap(&rq, input[i]);
-            i++;
-        }
+        while (index < n && processes[index].arrival <= time)
+            pushProcess(&rq, processes[index++]);
 
         if (rq.size == 0)
         {
-            int next_time = (i < n) ? input[i].arrival_time : time;
-            if (g_idx > 0 && gantt[g_idx - 1].pid == -1)
-                gantt[g_idx - 1].end = next_time;
-            else
-            {
-                gantt[g_idx].pid = -1;
-                gantt[g_idx].start = time;
-                gantt[g_idx].end = next_time;
-                g_idx++;
-            }
-            time = next_time;
+            int nextTime = processes[index].arrival;
+            chart[chartSize++] = (Gantt){-1, time, nextTime};
+            time = nextTime;
             continue;
         }
 
-        Process p = popHeap(&rq);
-        p.start_time = time;
-        p.response_time = time - p.arrival_time;
-        p.waiting_time = p.response_time;
-        gantt[g_idx].pid = p.pid;
-        gantt[g_idx].start = time;
-        time += p.burst_time;
-        p.completion_time = time;
-        p.turnaround_time = p.completion_time - p.arrival_time;
-        gantt[g_idx].end = time;
-        g_idx++;
-        total_wt  += p.waiting_time;
-        total_tat += p.turnaround_time;
-        total_rt  += p.response_time;
-        result[res_idx++] = p;
+        increaseAge(&rq);
+        Process current = popProcess(&rq);
+
+        current.start = time;
+        current.response = time - current.arrival;
+        current.waiting = current.response;
+
+        chart[chartSize++] = (Gantt){current.pid, time, time + current.burst};
+
+        time += current.burst;
+        current.completion = time;
+        current.turnaround = current.completion - current.arrival;
+
+        avgWait += current.waiting;
+        avgTurn += current.turnaround;
+        avgResp += current.response;
+
+        done++;
     }
 
-    printGanttChart(gantt, g_idx);
-    printf("Average Waiting Time: %.2f\n", total_wt / n);
-    printf("Average Turnaround Time: %.2f\n", total_tat / n);
-    printf("Average Response Time: %.2f\n", total_rt / n);
+    printGantt(chart, chartSize);
+
+    printf("Average Waiting Time    : %.2f\n", avgWait / n);
+    printf("Average Turnaround Time : %.2f\n", avgTurn / n);
+    printf("Average Response Time   : %.2f\n", avgResp / n);
 }
