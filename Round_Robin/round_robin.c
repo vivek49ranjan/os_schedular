703aefafa3f79642538a0b8155f444cd3afed87b
sakshamtiwari2006 <saksham4141tiwari@gmail.com>
2026-01-23 14:24:12 +0530

    Update round_robin.c

--- Round_Robin/round_robin.c
+++ Round_Robin/round_robin.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-struct process
+struct process_rr
 {
     char Pid[10];
     int Arrival_Time;
@@ -10,250 +11,206 @@ struct process
 };
 
 
-void enqueue(char queue[][10], int *front, int *rear, char value[10], int n)
+struct node_rr
 {
-    if(((*rear + 1) % n) == *front)
-        return;
-    else if(*front == -1){
-        *front = 0;
-        *rear = 0;
-        strcpy(queue[*rear], value);
-    }
-    else{
-        *rear = (*rear + 1) % n;
-        strcpy(queue[*rear], value);
-    }
-}
+    char pid[10];
+    struct node_rr *next;
+};
 
 
-void dequeue(char queue[][10], int *front, int *rear, int n)
+void enqueue_rr(struct node_rr **front, struct node_rr **rear, char val[10])
 {
-    if(*front == -1)
-        return;
-    else if(*front == *rear){
-        *front = -1;
-        *rear = -1;
-    }
-    else{
-        *front = (*front + 1) % n;
+    struct node_rr *temp = (struct node_rr*)malloc(sizeof(struct node_rr));
+    strcpy(temp->pid,val);
+    temp->next = NULL;
+
+    if(*rear == NULL){
+        *front = *rear = temp;
+    } else {
+        (*rear)->next = temp;
+        *rear = temp;
     }
 }
 
 
-
-bool empty(char queue[][10], int *front, int *rear, int n)
+void dequeue_rr(struct node_rr **front, struct node_rr **rear)
 {
-    if(*front == -1)
-        return 1;
-    return 0;
+    if(*front == NULL) return;
+
+    struct node_rr *temp = *front;
+    *front = (*front)->next;
+
+    if(*front == NULL)
+        *rear = NULL;
+
+    free(temp);
 }
 
 
-
-bool present_in_queue(char pid[10], char array[][10],int n)
+bool empty_rr(struct node_rr *front)
 {
-    for(int j=0;j<n;j++){
-        if(strcmp(array[j], pid) == 0){
-            return true;
-        }
-    }
-    return false;
+    return front == NULL;
 }
 
 
-
-void read_input(int n,char Pid_list[][10],int Arrival_time_list[],int Burst_time_list[])
+void round_robin(int n, struct process_rr p[],
+                 struct node_rr **front, struct node_rr **rear,
+                 int rem_bt[], char order[][10], int time[],
+                 int *c1, int *c2)
 {
-    int a,b;
-    char c[10];
+    int quantum;
+    int current_time = 0;
+    int completed = 0;
+    bool visited[n];
+    bool first_quantum = true; 
 
     for(int i=0;i<n;i++){
-        scanf("%s",c);
-        scanf("%d",&a);
-        scanf("%d",&b);
-        strcpy(Pid_list[i],c);
-        Arrival_time_list[i]=a;
-        Burst_time_list[i]=b;
+        rem_bt[i] = p[i].Burst_Time;
+        visited[i] = false;
     }
-}
 
+    time[0] = 0;
+    printf("\nQuantum sizes used: ");
 
-
-void fill_struct(int n,struct process p[],char Pid_list[][10],int Arrival_time_list[],int Burst_time_list[])
-{
-    for(int i=0;i<n;i++){
-        strcpy(p[i].Pid,Pid_list[i]);
-        p[i].Arrival_Time=Arrival_time_list[i];
-        p[i].Burst_Time=Burst_time_list[i];
-    }
-}
-
-
-
-void round_robin(int n,struct process p[],char queue[][10],int *front,int *rear,int remaining_burst_time[],char process_order[][10],int time[],int *count1,int *count2)
-{
-    int current_time=0;
-    int quantum_size=2;
-    int completed=0;
-
-    time[0]=0;
-
-    for(int i=0;i<n;i++)
-        remaining_burst_time[i]=p[i].Burst_Time;
-
-    for(int i=0;i<n;i++)
-        if(p[i].Arrival_Time==0)
-            enqueue(queue,front,rear,p[i].Pid,n);
-
-    while(completed<n)
+    while(completed < n)
     {
-        if(empty(queue,front,rear,n)){
-            current_time++;
-
-            for(int i=0;i<n;i++){
-                if(p[i].Arrival_Time<=current_time && remaining_burst_time[i]>0 && !present_in_queue(p[i].Pid,queue,n)){
-                    enqueue(queue,front,rear,p[i].Pid,n);
-                }
+        for(int i=0;i<n;i++){
+            if(p[i].Arrival_Time <= current_time && rem_bt[i] > 0 && !visited[i]){
+                enqueue_rr(front,rear,p[i].Pid);
+                visited[i] = true;
             }
+        }
+
+        if(empty_rr(*front)){
+            current_time++;
             continue;
         }
 
-        char current_pid[10];
-        strcpy(current_pid,queue[*front]);
-        dequeue(queue,front,rear,n);
+        if (first_quantum) {
+            quantum = 2;
+            first_quantum = false; 
+        } 
+        else {
+            double inv_sum = 0.0;
+            int cnt = 0;
+            struct node_rr *temp = *front;
+            while(temp){
+                for(int i=0;i<n;i++){
+                    if(strcmp(p[i].Pid,temp->pid)==0){
+                        if(rem_bt[i] > 0){
+                            inv_sum += 1.0 / rem_bt[i];
+                            cnt++;
+                        }
+                        break;
+                    }
+                }
+                temp = temp->next;
+            }
+            if(cnt > 0 && inv_sum > 0){
+                quantum = (int)(cnt / inv_sum);
+            }
+            else{
+                quantum = 2; 
+            }
+
+            if(quantum < 2){ 
+                quantum = 2;
+            }
+        }
+        printf("%d  ", quantum);
+
+        char cur[10];
+        strcpy(cur,(*front)->pid);
+        dequeue_rr(front,rear);
 
         int j;
-        for(j=0;j<n;j++)
-            if(strcmp(p[j].Pid,current_pid)==0)
+        for(j=0;j<n;j++){
+            if(strcmp(p[j].Pid,cur)==0){
                 break;
+            }
+        }
 
-        strcpy(process_order[*count1],p[j].Pid);
-        (*count1)++;
-        time[*count2]=current_time;
-        (*count2)++;
+        strcpy(order[*c1],cur);
+        (*c1)++;
+        time[*c2] = current_time;
+        (*c2)++;
 
-        if(remaining_burst_time[j]>quantum_size){
-            current_time+=quantum_size;
-            remaining_burst_time[j]-=quantum_size;
+        if(rem_bt[j] > quantum){
+            current_time += quantum;
+            rem_bt[j] -= quantum;
         }
         else{
-            current_time+=remaining_burst_time[j];
-            remaining_burst_time[j]=0;
+            current_time += rem_bt[j];
+            rem_bt[j] = 0;
             completed++;
         }
 
         for(int i=0;i<n;i++){
-            if(p[i].Arrival_Time<=current_time &&remaining_burst_time[i]>0 && !present_in_queue(p[i].Pid,queue,n)){
-                enqueue(queue,front,rear,p[i].Pid,n);
+            if(p[i].Arrival_Time <= current_time && rem_bt[i] > 0 && !visited[i]){
+                enqueue_rr(front,rear,p[i].Pid);
+                visited[i] = true;
             }
         }
 
-        if(remaining_burst_time[j]>0)
-            enqueue(queue,front,rear,p[j].Pid,n);
+        if(rem_bt[j] > 0){
+            enqueue_rr(front,rear,p[j].Pid);
+        }
     }
-
-    time[*count2]=current_time;
-    (*count2)++;
+    time[*c2] = current_time;
+    (*c2)++;
+    printf("\n");
 }
 
 
-void print_gantt(int count1,int count2,char process_order[][10],int time[])
+void print_gantt_rr(int c1,int c2,char order[][10],int time[])
 {
-    printf("\nGantt Chart:\n");
-    for(int i=0;i<count1;i++)
-        printf("  %s",process_order[i]);
-    printf("\n");
+    printf("\nRound Robin Gantt Chart:\n|");
+    for(int i=0;i<c1;i++){
+        printf(" %s |",order[i]);
+    }
 
-    for(int i=0;i<count2;i++){
+    printf("\n");
+    for(int i=0;i<c2;i++){
         if(time[i]<10){
-            printf("%d   ",time[i]);
+            printf("%d    ",time[i]);
         }
         else{
-            printf("%d  ",time[i]);
+            printf("%d   ",time[i]);
         }
     }
     printf("\n");
 }
 
-void calculate_metrics(int n,struct process p[],char process_order[][10],int time[],int count1)
-{
-    int CT[n];
-    int TAT[n];
-    int WT[n];
-    int RT[n];
-    int FET[n];
 
-    printf("\n      TAT    WT    RT   AT   BT\n");
+void calculate_metrics_rr(int n,struct process_rr p[],char order[][10],int time[],int c1)
+{
+    int CT[n],TAT[n],WT[n],RT[n];
+    printf("\nPID  AT   BT   CT   TAT    WT   RT\n");
 
     for(int i=0;i<n;i++){
-
-        CT[i] = -1;
-        for(int k=count1-1;k>=0;k--){
-            if(strcmp(process_order[k], p[i].Pid)==0){
+        for(int k=c1-1;k>=0;k--){
+            if(strcmp(order[k],p[i].Pid)==0){
                 CT[i] = time[k+1];
-                TAT[i] = CT[i]-p[i].Arrival_Time;
-                WT[i] = TAT[i]- p[i].Burst_Time;
+                TAT[i] = CT[i] - p[i].Arrival_Time;
+                WT[i] = TAT[i] - p[i].Burst_Time;
                 break;
             }
         }
-
-        FET[i]= -1;
-        for(int k =0; k<count1; k++){
-            if(strcmp(process_order[k], p[i].Pid)==0){
-                FET[i] = time[k];
-                RT[i] = FET[i]-p[i].Arrival_Time;
+        for(int k=0;k<c1;k++){
+            if(strcmp(order[k],p[i].Pid)==0){
+                RT[i] = time[k] - p[i].Arrival_Time;
                 break;
             }
         }
-
-        printf("%s-:  %d      %d    %d    %d    %d\n",
-               p[i].Pid,TAT[i],WT[i],RT[i],
-               p[i].Arrival_Time,p[i].Burst_Time);
+        printf("%s   %d    %d    %d    %d    %d    %d\n",
+               p[i].Pid,p[i].Arrival_Time,p[i].Burst_Time,
+               CT[i],TAT[i],WT[i],RT[i]);
     }
-
-    int sum_TAT=0,sum_RT=0,sum_WT=0;
-
-    for(int i=0; i<n; i++){
-        sum_TAT += TAT[i];
-        sum_WT += WT[i];
-        sum_RT += RT[i];
+    float s1=0,s2=0,s3=0;
+    for(int i=0;i<n;i++){
+        s1+=TAT[i]; s2+=WT[i]; s3+=RT[i];
     }
-
-    printf("\nAverage TAT-: %f\n",(float)sum_TAT/n);
-    printf("Average WT-: %f\n",(float)sum_WT/n);
-    printf("Average RT-: %f\n",(float)sum_RT/n);
-}
-
-
-
-int main()
-{
-    int n;
-    scanf("%d",&n);
-
-    char queue[n][10];
-    char pop_array[n][10];
-    int front=-1,rear=-1;
-
-    char Pid_list[n][10];
-    int Arrival_time_list[n];
-    int Burst_time_list[n];
-
-    read_input(n,Pid_list,Arrival_time_list,Burst_time_list);
-
-    struct process p[n];
-    fill_struct(n,p,Pid_list,Arrival_time_list,Burst_time_list);
-
-    int remaining_burst_time[n];
-    char process_order[100][10];
-    int time[100];
-    int count1=0,count2=0;
-
-    round_robin(n,p,queue,&front,&rear,remaining_burst_time,process_order,time,&count1,&count2);
-
-    print_gantt(count1,count2,process_order,time);
-
-    calculate_metrics(n,p,process_order,time,count1);
-
-    return 0;
+    printf("\nAverage TAT = %.2f\n",s1/n);
+    printf("Average WT  = %.2f\n",s2/n);
+    printf("Average RT  = %.2f\n",s3/n);
 }
